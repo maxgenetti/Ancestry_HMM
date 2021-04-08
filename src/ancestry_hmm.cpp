05a410a26018e1cd3b83bd28c69e3fe607030696
maxgenetti <43384843+maxgenetti@users.noreply.github.com>
2021-04-08 12:35:55 -0600

    Add files via upload

--- src/ancestry_hmm.cpp
+++ src/ancestry_hmm.cpp
@@ -36,6 +36,7 @@ using namespace arma ;
 #include "read_samples.h" 
 #include "pulses_to_ancestry.h" 
 #include "compute_forward.h"
+#include "compute_forward_subset.h" /// edit max - added new forward calculation
 #include "compute_backward.h"
 #include "forward_backward.h"
 #include "viterbi.h" 
@@ -97,7 +98,7 @@ int main ( int argc, char *argv[] ) {
     vector<int> position ;
     vector<double> recombination_rate ;
     vector<string> chromosomes ;
-    double total_recombination ;
+    double total_recombination ; /// edit max - added variable total_recombination
     read_file( options, markov_chain_information, state_list, position, recombination_rate, total_recombination, chromosomes ) ; /// edit max - added variable total_recombination
 
     /// create basic transition information
@@ -185,7 +186,7 @@ int main ( int argc, char *argv[] ) {
     /// create transition rates for the optimal or supplied set of pulses
     cerr << endl << "creating per morgan transition rates\t\t" ; t = clock();
     mat transition_rates = create_transition_rates( optimum, options.ne, options.ancestry_proportion ) ;
-    
+
     /// create transition information
     cerr << (double) (clock() - t) << " ms" << endl << "creating transition matrices\t\t\t" ; t = clock();
     map<int,vector<mat> > transition_matrix ;
@@ -195,7 +196,8 @@ int main ( int argc, char *argv[] ) {
             create_transition_matrix( transition_matrix, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate, position, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
         }
     }
-    
+
+
     //// create interploidy transition matrix
     vector<mat> interploidy_transitions = create_interploidy_transitions ( state_list, optimum, options.ancestry_proportion ) ;
     
@@ -207,6 +209,146 @@ int main ( int argc, char *argv[] ) {
         }
     }
 
+    /// edit Max - begin
+    /// output recombination map after using golden search on forward-backward probabilities
+    else if ( options.recombination_map == true ) {
+        
+        ///cout << markov_chain_information.size() << "\t:0\n";
+        ///cout << optimum.size() << "\t:1\n";
+        ///cout << transition_rates.size() << "\t:2\n";
+        ///cout << transition_rates[1] << "\t:3\n";
+        ///cout << interploidy_transitions.size() << "\t:4\n";
+        ///cout << transition_matrix[2][0] << "\t:5\n" ;
+        ///cout << markov_chain_information.size() << "\t:6\n" ;
+      
+        ///create dictionaries to define regions to optimize around site, 0.5mb behind and 0.5mb ahead
+        map<int, int> nBound;
+        map<int, int> lBound;
+        for ( int m = 0 ; m < position.size() ; m ++) { ///iterate through all positions
+            nBound[m] = m ;
+            lBound[m] = m ;
+            for ( int n = m + 1; n < position.size() && position[n] > position[n-1]; n++){ ///iterate through all forward positions on same chromsome "position[n] > position[n-1]"
+                if (position[n] < position[m] + 500000){ 
+                    nBound[m] = n ;
+                }
+            }
+            for ( int l = m - 1; l >= 0 && position[l] < position[l + 1]; l--){ ///iterate through all reverse positions on same chromsome "position[l] < position[l + 1]"
+                if (position[l] > position[m] - 500000){ 
+                    lBound[m] = l ;
+                }
+            }
+        }
+
+        static const double golden = 1.6180339887498 ;
+        static const double minRecombination = 1e-10 ;
+        static const double maxRecombination = 1e-5 ;
+        static const double tolerance = 1e-10 ;
+        vector<double> recombination_rate_low;
+        vector<double> recombination_rate_high;
+        vector<double> recombination_rate_a; ///trash
+        vector<double> recombination_rate_b; ///trash
+        vector<double> recombination_rate_c; ///trash
+        
+        ///iterative over each site
+        ///for (int i = 1000; i < 1010 ; i ++) {   ///test range
+        for (int i = 0; i < position.size() ; i ++) {
+            ///vector<double> recombination_rate_subset(recombination_rate.begin() + lBound[i], recombination_rate.begin() + nBound[i]) ; ///subset of local recombination rate
+            vector<int> position_subset(position.begin() + lBound[i], position.begin() + nBound[i]) ; ///subset of local positions
+
+            double a = 5 ;
+            double b = 10 ;
+            double c = b - (b - a) / golden ;
+            double d = a + (b - a) / golden ;
+            double x = 0 ;
+            double y = 1 ;
+            ///cout << "starting at site: " << i << "\n" ;
+            /* something is fucking broken
+
+            ///testing whats fucked
+            recombination_rate_a.assign(position_subset.size(), a);
+            recombination_rate_b.assign(position_subset.size(), b);
+            recombination_rate_c.assign(position_subset.size(), c);
+
+
+            map<int,vector<mat> > transition_matrix_1 ;
+            map<int,vector<mat> > transition_matrix_2 ;
+            map<int,vector<mat> > transition_matrix_3 ;
+            for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
+                create_transition_matrix( transition_matrix_1, transition_matrix_information[markov_chain_information.at(m).number_chromosomes], recombination_rate_a, position_subset, markov_chain_information.at(m).number_chromosomes, transition_rates ) ;
+                create_transition_matrix( transition_matrix_2, transition_matrix_information[markov_chain_information.at(m).number_chromosomes], recombination_rate_b, position_subset, markov_chain_information.at(m).number_chromosomes, transition_rates ) ;
+                create_transition_matrix( transition_matrix_3, transition_matrix_information[markov_chain_information.at(m).number_chromosomes], recombination_rate_c, position_subset, markov_chain_information.at(m).number_chromosomes, transition_rates ) ;
+                for ( int p = 0 ; p < markov_chain_information[m].ploidy_switch.size() ; p ++ ) {
+                    create_transition_matrix( transition_matrix_1, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate_a, position_subset, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
+                    create_transition_matrix( transition_matrix_2, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate_b, position_subset, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
+                    create_transition_matrix( transition_matrix_3, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate_c, position_subset, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
+                }
+            }
+
+
+            x = 0 ;
+            y = 0 ;
+            double z = 0 ;
+            for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
+                vector<vec> emission_probabilities_subset(markov_chain_information[m].emission_probabilities.begin() + lBound[i], markov_chain_information[m].emission_probabilities.begin() + nBound[i]) ;            
+                x += markov_chain_information[m].compute_forward_probabilities_subset( transition_matrix_1, interploidy_transitions, emission_probabilities_subset ) ;
+                y += markov_chain_information[m].compute_forward_probabilities_subset( transition_matrix_2, interploidy_transitions, emission_probabilities_subset ) ;
+                z += markov_chain_information[m].compute_forward_probabilities_subset( transition_matrix_3, interploidy_transitions, emission_probabilities_subset ) ;
+            }
+            */
+            for (int iters = 0; iters < 1000 && abs(a-b)>tolerance; iters ++) {    /// gss at each site
+
+                recombination_rate_low.assign(position_subset.size(), pow(10,-1*c));
+                recombination_rate_high.assign(position_subset.size(), pow(10,-1*d));
+
+                /// create transition information
+                map<int,vector<mat> > transition_matrix_1 ;
+                map<int,vector<mat> > transition_matrix_2 ;
+                for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
+                    create_transition_matrix( transition_matrix_1, transition_matrix_information[markov_chain_information.at(m).number_chromosomes], recombination_rate_low, position_subset, markov_chain_information.at(m).number_chromosomes, transition_rates ) ;
+                    create_transition_matrix( transition_matrix_2, transition_matrix_information[markov_chain_information.at(m).number_chromosomes], recombination_rate_high, position_subset, markov_chain_information.at(m).number_chromosomes, transition_rates ) ;
+                    for ( int p = 0 ; p < markov_chain_information[m].ploidy_switch.size() ; p ++ ) {
+                        create_transition_matrix( transition_matrix_1, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate_low, position_subset, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
+                        create_transition_matrix( transition_matrix_2, transition_matrix_information[markov_chain_information[m].ploidy_switch[p]], recombination_rate_high, position_subset, markov_chain_information[m].ploidy_switch[p], transition_rates ) ;
+                    }
+                }
+
+                /// calculate forward probabilities
+                x = 0 ;
+                y = 0 ;
+                for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
+                    vector<vec> emission_probabilities_subset(markov_chain_information[m].emission_probabilities.begin() + lBound[i], markov_chain_information[m].emission_probabilities.begin() + nBound[i]) ;            
+                    x += markov_chain_information[m].compute_forward_probabilities_subset( transition_matrix_1, interploidy_transitions, emission_probabilities_subset ) ;
+                    y += markov_chain_information[m].compute_forward_probabilities_subset( transition_matrix_2, interploidy_transitions, emission_probabilities_subset ) ;
+
+                }
+
+                if (x < y){
+                    b = d ;
+                }
+                else{
+                    a = c ;
+                }
+                c = b - (b - a) / golden ;
+                d = a + (b - a) / golden ;
+                
+                ///cout << "a and x : " << a << " and "<< x <<"\n" ;
+                ///cout << "b and y : " << b << " and "<< y <<"\n" ;
+                ///cout << markov_chain_information.size() << "\t:6b\n";
+                ///cout << markov_chain_information[0].emission_probabilities.size() << "\t: emission probabilities size in main\n";
+
+                ///cout << "a : " << a <<"\n" ;
+                ///cout << "b : " << b <<"\n" ;           
+                ///cout << "x : " << x <<"\n" ;
+                ///cout << "y : " << y <<"\n" ; 
+            }
+            ///cout << "recombination rate at position " << position[i] << " is :" << pow(10, -(a+b)/2) <<"\n" ;
+            cout << position[i] << "\t" << pow(10, -(a+b)/2) <<"\n" ;
+        }
+
+    }
+    /// edit Max - end
+
+
     /// output forward-backward full probability distribution by default
     else {
         cerr << (double) (clock() - t) << " ms" << endl << "computing forward probabilities\t" ; t = clock() ;
@@ -214,7 +356,7 @@ int main ( int argc, char *argv[] ) {
         for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
             lnl += markov_chain_information[m].compute_forward_probabilities( transition_matrix, interploidy_transitions ) ;
         }
-        
+
         cerr << "lnl: " << lnl << "\t\t" << (double) (clock() - t) << " ms" << endl << "computing backward probabilities\t\t\t" ; t = clock() ;
         for ( int m = 0 ; m < markov_chain_information.size() ; m ++ ) {
             markov_chain_information[m].compute_backward_probabilities( transition_matrix, interploidy_transitions ) ;
@@ -225,7 +367,9 @@ int main ( int argc, char *argv[] ) {
             markov_chain_information[m].combine_prob( position, state_list, chromosomes, options.output_pulses, optimum ) ;
         }
     }
-        
+    
+
+
     cerr << (double) (clock() - t) << " ms" << endl ;
     cerr << "total run time:\t\t\t" << (double) (clock() - total) << " ms" << endl ;
 
